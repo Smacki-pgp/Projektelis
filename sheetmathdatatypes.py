d3b9a002a82412ce8fd24117261a5fedb82f1892
Smacki-pgp <jocysmodestas1@gmail.com>
2024-11-22 20:22:18 +0200

    Practice to remmember python essentials in exercise1 and sheetmathdatatypes

---
+++ sheetmathdatatypes.py
@@ -0,0 +1,38 @@
+# Introduction
+# Day 1 - Remmembering python
+
+print(3 + 2)   # addition(+)
+print(3 - 2)   # subtraction(-)
+print(3 * 2)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Ricardo'))           # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Lucy'}))     # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(3 == 3))              # Bool
+print(type(3 >= 3))              # Bool
+
+
+first_name = "John"
+
+# Printing out types
+print(type('Stringy'))           # str
+print(type(first_name))          # str
+print(type(10))                  # int
+print(type(3.14))                # float
+print(type(1 + 1j))              # complex
+print(type(True))                # bool
+print(type([1, 2, 3, 4]))        # list
+print(type({'name':'Lucy'}))     # dict
+print(type((1,2)))               # tuple
+print(type(zip([1,2],[3,4])))    # zip
