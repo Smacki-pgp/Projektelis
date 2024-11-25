995c163fe33d2341699b3edffd9787558cd0391f
Smacki-pgp <jocysmodestas1@gmail.com>
2024-11-25 06:07:10 +0200

    Remmembering some string formatting

--- more_on_strings.py
+++ more_on_strings.py
@@ -1,3 +1,12 @@
+import datetime
+current_time = datetime.datetime.now()
+print("it's now: {:%H:%M:%S}".format(current_time))
+user_name = input("What is your name..? ")
+user_age = input("How old are you? ")
+seconds_lived = user_age*365*24*60*60
+print(f"Hello {user_name}, how have you been? Based on your age input, you have lived for {seconds_lived} seconds... The clock is ticking")
+
+
 print("\n Lately it has been a blast remmembering python. \n Hasn't it?\n")
 
 # 8 spaces reserved, 3 spaces after decimal
